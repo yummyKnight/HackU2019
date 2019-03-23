a622a06e5134fd57aba35b4f046235e91cbc181b
Nick <sshedl22@gmail.com>
2019-03-23 09:59:59 +0300

    Node  + Machine

--- MNode.py
+++ MNode.py
@@ -1,2 +1,4 @@
 class MNode:
-    pass
+    form = str()
+    arrayOfMachine = [] 
+
---
+++ Machine.py
@@ -0,0 +1,6 @@
+class Machine:
+    id = int()
+    form = str()
+    speed = int()
+    history = []  # dinamic array of logs
+    state = bool()
